d3149faedabf34ae0065f7f67b274c104ec269a0
zequihg50 <ezequiel.cimadevilla@unican.es>
2023-10-24 17:56:48 +0200

    split tests

--- test/test.py
+++ test/test.py
@@ -1,16 +1,15 @@
 import unittest
 
 import netCDF4
-from sqlalchemy.orm import Session
-
-from smgdatatools.model.model import Base, Store
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.pool import QueuePool
 
 from smgdatatools.collector.nc import NcCollector
 from smgdatatools.collector.zarr import ZarrCollector
 from smgdatatools.etl.h5vds import Common, NewCommon, New, Union
 from smgdatatools.etl.jinja import JinjaEtl
+from smgdatatools.model.model import Base, Store
 
 
 def parse_coord_values_attr(coord_values_attr_spec, stores):
@@ -23,6 +22,94 @@ def parse_coord_values_attr(coord_values_attr_spec, stores):
                     values.add(attr.value)
 
     return list(values)
+
+
+class TestGCSZarr(unittest.TestCase):
+    def test_gcs_zarr_ensemble(self):
+        datasets = [
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/tas/gn/v20191120",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/tas/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/tas/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/pr/gn/v20191120",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/pr/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/pr/gn/v20200226",
+        ]
+
+        collector = ZarrCollector()
+
+        engine = create_engine(
+            "sqlite+pysqlite:///:memory:",
+            echo=False,
+            future=True,
+            poolclass=QueuePool,
+            pool_size=1)
+        Base.metadata.create_all(engine)
+        session = Session(engine)
+
+        for store in (collector.collect(x) for x in datasets):
+            session.add(store)
+
+        stores = session.query(Store).all()
+
+        etl = JinjaEtl("gcs-cmip6.json.j2", dict())
+        # etl = JinjaEtl("test.j2", dict())
+        etl.run(
+            "gcs.json",
+            collector,
+            stores,
+            ["tas", "pr"])
+
+        # import xarray
+        # ds = xarray.open_dataset("reference://", decode_cf=False, engine="zarr", backend_kwargs={
+        #     "consolidated": False,
+        #     "storage_options": {"fo": 'test/gcs.json', "remote_protocol": "gs",
+        #                         "remote_options": {"anon": True}}
+        # })
+        # print(ds)
+
+    def test_gcs_zarr_ensemble2(self):
+        datasets = [
+            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r1i1p1f1/Amon/tas/gn/v20210318",
+            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r2i1p1f1/Amon/tas/gn/v20210318",
+            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r1i1p1f1/Amon/pr/gn/v20210318",
+            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r2i1p1f1/Amon/pr/gn/v20210318",
+        ]
+
+        collector = ZarrCollector()
+        stores = [collector.collect(x) for x in datasets]
+        etl = JinjaEtl("gcs-cmip6.json.j2", dict())
+        etl.run(
+            "gcs.json",
+            collector,
+            stores,
+            ["tas", "pr"])
+
+    def test_gcs_zarr_ensemble3(self):
+        datasets = [
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/pr/gn/v20191120",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/ta/gn/v20191120",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/tas/gn/v20191120",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/zg/gn/v20191120",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/pr/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/ta/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/tas/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/zg/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/pr/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/ta/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/tas/gn/v20200226",
+            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/zg/gn/v20200226",
+
+        ]
+
+        collector = ZarrCollector()
+        stores = [collector.collect(x) for x in datasets]
+        aggs = parse_coord_values_attr("variable_id", stores)
+        etl = JinjaEtl("gcs-cmip6.json.j2", dict())
+        etl.run(
+            "gcs.json",
+            collector,
+            stores,
+            aggs)
 
 
 class TestEsgfVds(unittest.TestCase):
@@ -157,92 +244,6 @@ class TestEsgfVds(unittest.TestCase):
 
         ds.close()
 
-    def test_gcs_zarr_ensemble(self):
-        datasets = [
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/tas/gn/v20191120",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/tas/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/tas/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/pr/gn/v20191120",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/pr/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/pr/gn/v20200226",
-        ]
-
-        collector = ZarrCollector()
-
-        engine = create_engine(
-            "sqlite+pysqlite:///:memory:",
-            echo=False,
-            future=True,
-            poolclass=QueuePool,
-            pool_size=1)
-        Base.metadata.create_all(engine)
-        session = Session(engine)
-
-        for store in (collector.collect(x) for x in datasets):
-            session.add(store)
-
-        stores = session.query(Store).all()
-
-        etl = JinjaEtl("gcs-cmip6.json.j2", dict())
-        #etl = JinjaEtl("test.j2", dict())
-        etl.run(
-            "gcs.json",
-            collector,
-            stores,
-            ["tas", "pr"])
-
-        # import xarray
-        # ds = xarray.open_dataset("reference://", decode_cf=False, engine="zarr", backend_kwargs={
-        #     "consolidated": False,
-        #     "storage_options": {"fo": 'test/gcs.json', "remote_protocol": "gs",
-        #                         "remote_options": {"anon": True}}
-        # })
-        # print(ds)
-
-    def test_gcs_zarr_ensemble2(self):
-        datasets = [
-            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r1i1p1f1/Amon/tas/gn/v20210318",
-            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r2i1p1f1/Amon/tas/gn/v20210318",
-            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r1i1p1f1/Amon/pr/gn/v20210318",
-            "gs://cmip6/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp585/r2i1p1f1/Amon/pr/gn/v20210318",
-        ]
-
-        collector = ZarrCollector()
-        stores = [collector.collect(x) for x in datasets]
-        etl = JinjaEtl("gcs-cmip6.json.j2", dict())
-        etl.run(
-            "gcs.json",
-            collector,
-            stores,
-            ["tas", "pr"])
-
-    def test_gcs_zarr_ensemble3(self):
-        datasets = [
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/pr/gn/v20191120",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/ta/gn/v20191120",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/tas/gn/v20191120",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/zg/gn/v20191120",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/pr/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/ta/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/tas/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r2i1p1f1/Amon/zg/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/pr/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/ta/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/tas/gn/v20200226",
-            "gs://cmip6/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r3i1p1f1/Amon/zg/gn/v20200226",
-
-        ]
-
-        collector = ZarrCollector()
-        stores = [collector.collect(x) for x in datasets]
-        aggs = parse_coord_values_attr("variable_id", stores)
-        etl = JinjaEtl("gcs-cmip6.json.j2", dict())
-        etl.run(
-            "gcs.json",
-            collector,
-            stores,
-            aggs)
-
     def test_cmip6_new_common_vds_year2300(self):
         datasets = [
             # r1i1p1f1, tas and pr, 1850-1869 and 1870-1889
@@ -322,6 +323,7 @@ class TestEsgfVds(unittest.TestCase):
 
         session.close()
         engine.dispose()
+
 
 if __name__ == "__main__":
     unittest.main()
